0a369d9c6bd1cde1f21f986a6fd2a6a74346bd66
LucasPairetti <ljpairetti@gmail.com>
2023-10-24 21:58:01 -0300

    primer commit del tp

---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+module Inmobiliaria {
+	requires javafx.controls;
+    requires java.sql;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires java.desktop;
+    
+
+    opens application to javafx.graphics, javafx.fxml, javafx.base, org.hibernate.core, hibernate.jpa, jboss.annotations, jboss.jms.api,
+    jboss.logging, jboss.transaction.api;
+
+}
