523e9954c70d6c7a519c5107693714fb24126bc3
Sofia N <sofia.30.10.99@gmail.com>
2023-11-12 23:06:00 -0300

    ventaservices

---
+++ src/services/VentaServices.java
@@ -0,0 +1,53 @@
+package services;
+
+import java.util.List;
+
+import Controllers.Validation;
+import application.clases.Vendedor;
+import application.clases.Venta;
+import application.dao.ClienteDAO;
+import application.dao.InmuebleDAO;
+import application.dao.VendedorDAO;
+import application.dao.VentaDAO;
+
+public class VentaServices {
+	VentaDAO ventadao;
+	InmuebleDAO inmuebledao;
+	VendedorDAO vendedordao;
+	ClienteDAO clientedao;
+	Validation validation;
+	public VentaServices() {
+		super();
+		ventadao = VentaDAO.getVentaDAO();
+		inmuebledao = InmuebleDAO.getVentaDAO();
+		validation = Validation.getInstance();
+	}
+	public void createVenta(Venta venta) {
+		//check todos existen
+		if(inmuebledao.getInmuebleById(venta.getPropiedad().getId())!=null 
+				/* && vendedordao.getVendedorById(venta.getVendedor().getId())!= null &&
+				clientedao.getClienteById(venta.getCliente().getId()!= null)*/) {
+			ventadao.createVenta(venta);
+		}
+		else {
+			//no existe el vendedor o el cliente o el inmueble
+		}
+	}
+	public void updateVenta(Venta v) { 
+		ventadao.updateVenta(v);
+	}
+	public void deleteVenta(Venta venta) {
+		ventadao.deleteVenta(venta);
+	}
+	public Venta getVentaById(int id) {
+		return ventadao.getVentaById(id);
+	}
+	
+	public List<Venta> getAllVentas(){
+		return ventadao.getAllVentas();
+	}
+	public List<Venta> getVentasByVendedor(Vendedor v){
+		return ventadao.getVentasByVendedor(v);
+	}
+	
+}
