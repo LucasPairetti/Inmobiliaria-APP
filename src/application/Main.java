d6856ccd1012c98692a9b7f41e58a61625cdd668
LucasPairetti <ljpairetti@gmail.com>
2023-12-09 12:31:27 -0300

    arreglo bd y otros cambios

--- src/application/Main.java
+++ src/application/Main.java
@@ -38,11 +38,18 @@ Parent root = FXMLLoader.load((getClass().getResource("/interfaces/PantallaPrinc
 	}
 	
 	public static void main(String[] args) {
-		//launch(args);
+	/*	
 		InmuebleDAO inmuebleDAO = InmuebleDAO.getInmuebleDAO();
 		Propietario p = new Propietario("Lucas", "Pairetti", TipoDNI.DNI,"42870675", "asd", 1111, "Santa Fe", Provincia.Santa_Fe,1111111,"pairet@gmail.com");
 		Inmueble inmueble = new Inmueble(p, Date.valueOf("2000-11-03"),true,Provincia.Santa_Fe,"Santa Fe","asd",2476,"","Fomento",
 				TipoInmueble.C,20000000, Orientacion.Norte,256,1,1,50,3,3,true,true,true,true,true,true,true,true,true,true,true,"Fachera");
 		inmuebleDAO.createInmueble(inmueble);
+		*/
+		
+		launch(args);	
+		
+		
+		
+		
 	}
 }
