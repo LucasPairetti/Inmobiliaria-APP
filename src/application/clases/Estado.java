afb04149883608abd8089c421606a31b64114fad
TomasAuday <audaytomas@gmail.com>
2023-12-09 14:25:29 -0300

    comienzo cambios estado

--- src/application/clases/Estado.java
+++ src/application/clases/Estado.java
@@ -1,15 +1,5 @@
 package application.clases;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public enum Estado {
-	Disponible,Reservado,Vendido;
-	public static List<String> getEstado(){
-		 return  Arrays.stream(Estado.values())
-	                .map(Enum::toString)
-	                .map(s -> s.replace("_", " "))
-	                .collect(Collectors.toList());
-	 }
+
 }
