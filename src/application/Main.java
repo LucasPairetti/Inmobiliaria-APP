16783e337e976fae25b2c2d0e5a379251c918546
TomasDannunzio <tomidannunzio@gmail.com>
2023-12-09 11:56:01 -0300

    Problema BDD - Solucionado

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,18 @@
 package application;
 	
+import java.sql.Date;
+
+import application.clases.Inmueble;
+import application.clases.Orientacion;
+import application.clases.Propietario;
+import application.clases.Provincia;
+import application.clases.TipoDNI;
+import application.clases.TipoInmueble;
+import application.dao.InmuebleDAO;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import services.InmuebleServices;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -28,6 +38,11 @@ Parent root = FXMLLoader.load((getClass().getResource("/interfaces/PantallaPrinc
 	}
 	
 	public static void main(String[] args) {
-		launch(args);
+		//launch(args);
+		InmuebleDAO inmuebleDAO = InmuebleDAO.getInmuebleDAO();
+		Propietario p = new Propietario("Lucas", "Pairetti", TipoDNI.DNI,"42870675", "asd", 1111, "Santa Fe", Provincia.Santa_Fe,1111111,"pairet@gmail.com");
+		Inmueble inmueble = new Inmueble(p, Date.valueOf("2000-11-03"),true,Provincia.Santa_Fe,"Santa Fe","asd",2476,"","Fomento",
+				TipoInmueble.C,20000000, Orientacion.Norte,256,1,1,50,3,3,true,true,true,true,true,true,true,true,true,true,true,"Fachera");
+		inmuebleDAO.createInmueble(inmueble);
 	}
 }
--- src/application/clases/Cliente.java
+++ src/application/clases/Cliente.java
@@ -1,6 +1,14 @@
 package application.clases;
 
-import javax.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 
 @Entity
 @Table(name="Cliente")
