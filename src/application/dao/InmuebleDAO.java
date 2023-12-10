4b748753a93905c739db1ef88fb691e3729f43cd
TomasDannunzio <tomidannunzio@gmail.com>
2023-12-09 21:23:28 -0300

    Consultas de Inmueble complejas terminadas, falta testeo

--- src/application/dao/InmuebleDAO.java
+++ src/application/dao/InmuebleDAO.java
@@ -134,7 +134,7 @@ public class InmuebleDAO {
 		}
 
 		
-			public List<Inmueble> getInmueble(Provincia provincia, String localidad, String barrio, TipoInmueble tipo, int dormitorios, 
+			public List<Inmueble> getInmueble(Provincia provincia, String localidad, String barrio, TipoInmueble tipo, Integer dormitorios, 
 					float precioMin, float precioMax) {
 				// TODO Auto-generated method stub
 				
@@ -143,21 +143,21 @@ public class InmuebleDAO {
 				
 				try {
 				
-				Query<Inmueble> query = session.createQuery("SELECT * FROM INMUEBLE WHERE"
-						+ "(PROVINCIA = ?1 OR ?1 IS NULL) AND"
-						+ "(LOCALIDAD = ?2 OR ?2 IS NULL) AND"
-						+ "(BARRIO = ?3 OR ?3 IS NULL) AND"
-						+ "(TIPO = ?4 OR ?4 IS NULL) AND"
-						+ "(DORMITORIOS = ?5 OR ?5 IS NULL) AND"
-						+ "(PRECIO>=?6 OR ?6 IS NULL) AND"
-						+ "(PRECIO<=?7 OR ?7 IS NULL);", Inmueble.class);
-				query.setParameter(1, provincia);
-				query.setParameter(2, localidad);
-				query.setParameter(3, barrio);
-				query.setParameter(4, tipo);
-				query.setParameter(5, dormitorios);
-				query.setParameter(6, precioMin);
-				query.setParameter(7, precioMax);
+				Query<Inmueble> query = session.createQuery("SELECT a FROM Inmueble a WHERE "
+						+ "(a.provincia = :provincia OR :provincia IS NULL) AND "
+						+ "(a.localidad = :localidad OR :localidad IS NULL) AND "
+						+ "(a.barrio = :barrio OR :barrio IS NULL) AND "
+						+ "(a.tipoInmueble = :tipo OR :tipo IS NULL) AND "
+						+ "(a.dormitorio = :dormitorio OR :dormitorio IS NULL) AND "
+						+ "(a.precio >= :precio OR :precio IS NULL) AND "
+						+ "(a.precio <= :precio OR :precio IS NULL)", Inmueble.class);
+				query.setParameter("provincia", provincia);
+				query.setParameter("localidad", localidad);
+				query.setParameter("barrio", barrio);
+				query.setParameter("tipo", tipo);
+				query.setParameter("dormitorios", dormitorios);
+				query.setParameter("precioMin", precioMin);
+				query.setParameter("precioMax", precioMax);
 				
 			    ArrayList<Inmueble> inmuebles = (ArrayList<Inmueble>) query.getResultList();
 			    
@@ -175,7 +175,7 @@ public class InmuebleDAO {
 				
 				
 			}
-			public List<Inmueble> getInmueble(Provincia provincia, String localidad,String calle, int numero,
+			public List<Inmueble> getInmueble(Provincia provincia, String localidad,String calle, Integer numero,
 					String pisodpto, TipoInmueble tipoInmueble) {
 				
 				Session session = HibernateUtil.getSessionFactory().getCurrentSession();
@@ -183,19 +183,19 @@ public class InmuebleDAO {
 				
 				try {
 				
-				Query<Inmueble> query = session.createQuery("SELECT * FROM INMUEBLE WHERE"
-						+ "(PROVINCIA = ?1 OR ?1 IS NULL) AND"
-						+ "(LOCALIDAD = ?2 OR ?2 IS NULL) AND"
-						+ "(CALLE = ?3 OR ?3 IS NULL) AND"
-						+ "(NUMERO = ?4 OR ?4 IS NULL) AND"
-						+ "(PISODPTO = ?5 OR ?5 IS NULL) AND"
-						+ "(TIPO = ?6 OR ?6 IS NULL)", Inmueble.class);
-				query.setParameter(1, provincia);
-				query.setParameter(2, localidad);
-				query.setParameter(3, calle);
-				query.setParameter(4, numero);
-				query.setParameter(5, pisodpto);
-				query.setParameter(6, tipoInmueble);
+				Query<Inmueble> query = session.createQuery("SELECT a FROM Inmueble a WHERE "
+						+ "(a.provincia = :provincia OR :provincia IS NULL) AND "
+						+ "(a.localidad = :localidad OR :localidad IS NULL) AND "
+						+ "(a.calle  = :calle OR :calle IS NULL) AND "
+						+ "(a.numero = :numero OR :numero IS NULL) AND "
+						+ "(a.pisodpto = :pisodpto OR :pisodpto IS NULL) AND "
+						+ "(a.tipoInmueble = :tipo OR :tipo IS NULL)", Inmueble.class);
+				query.setParameter("provincia", provincia);
+				query.setParameter("localidad", localidad);
+				query.setParameter("calle", calle);
+				query.setParameter("numero", numero);
+				query.setParameter("pisodpto", pisodpto);
+				query.setParameter("tipo", tipoInmueble);
 				
 			    ArrayList<Inmueble> inmuebles = (ArrayList<Inmueble>) query.getResultList();
 			    
